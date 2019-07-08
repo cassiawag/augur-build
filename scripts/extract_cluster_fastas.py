94ad9a545848073aba82ec69cc289ac533a5f3ed
Trevor Bedford <trevor@bedford.io>
2019-07-07 17:10:42 -0700

    Reorganize folder structure to split "segments" from "clusters"
    I wanted to cleanly split a workflow for segments from a workflow for clusters. The intention is to first run the Snakefile_segments and then run the Snakefile_clusters. Segments uses "segments-data/", "segments-results" and "segments-auspice/".

--- scripts/extract_cluster_fastas.py
+++ scripts/extract_cluster_fastas.py
@@ -1,8 +1,6 @@
-
 '''
 Creates full-genome fasta files for each clusters.
 '''
-import os
 import json
 import argparse
 
@@ -46,7 +44,7 @@ Outputs python dictionary sequences into fasta files
 def genomes(seq_dict, output):
     for cluster, cluster_strains in seq_dict.items():
         new_output = output.replace('0', cluster)
-        with open (new_output, "w") as fasta: 
+        with open (new_output, "w") as fasta:
             for strain, seq in cluster_strains.items():
                 fasta.write('>' + strain + ' | cluster'+cluster + '\n' + seq + '\n')
     return fasta
@@ -57,16 +55,16 @@ if __name__ == '__main__':
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
-    parser.add_argument('--clusters', type = str, required=True, help = "cluster JSON files")
-    parser.add_argument('--nt-muts', nargs='+', type =str, required=True, help = "list of nt-muts JSON files")
-    parser.add_argument('--output', type = str, required=True, help = "output location")
+    parser.add_argument('--clusters', type = str, required = True, help = "cluster JSON files")
+    parser.add_argument('--nt-muts', nargs = '+', type = str, required = True, help = "list of nt-muts JSON files")
+    parser.add_argument('--output', type = str, required = True, help = "output location")
     args = parser.parse_args()
 
-    #Create clusters dictionary
+    # Create clusters dictionary
     cluster_dict = clusters(args.clusters)
 
-    #Makes sequences dictionary
+    # Makes sequences dictionary
     cluster_seq_dict = sequences(cluster_dict, args.nt_muts)
 
-    #Outputs fasta files
+    # Outputs fasta files
     genomes(cluster_seq_dict, args.output)
