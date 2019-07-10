f3b2a13d3826a3adf2d4429a930ac3311c726b14
Cassia Wagner <cassia.wagner9@gmail.com>
2019-07-10 10:27:25 -0700

    Updated H3N2 reference to A/Perth/16/2009.

--- scripts/create_ref_genome.py
+++ scripts/create_ref_genome.py
@@ -14,6 +14,16 @@ def concat(files):
             flu_ref = seq
         else:
             flu_ref += seq
+    ha = SeqIO.read(files[0], 'gb')
+    descrip = ha.annotations['source']
+    name = descrip.split('(', 2)[1]
+    #Genbank forces names to be 16 characters or less.
+    if 'California' in name:
+        flu_ref.name = name.replace('California', 'Cali')
+    else:
+        flu_ref.name = name
+    flu_ref.description = descrip
+    flu_ref.id = descrip
     return flu_ref
 
 if __name__ == '__main__':
@@ -26,5 +36,5 @@ if __name__ == '__main__':
     parser.add_argument('--output', type = str, required = True, help = "output location")
     args = parser.parse_args()
 
-#Writes out reference genbank file for full influenza genome     
+# Writes out reference genbank file for full influenza genome     
 SeqIO.write(concat(args.references), args.output, 'gb')
