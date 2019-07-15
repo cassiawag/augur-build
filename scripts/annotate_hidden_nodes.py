bb676b49fb53d5509ec2337a88540bff95d59b5f
james hadfield <jh22@sanger.ac.uk>
2019-07-15 12:55:18 +1200

    Hide parent-cluster branch & maximise screen space
    (1) hide the branch from a (hidden) parent to the common ancestor of
    a (monophyletic) cluster. This involves adding a "stub" node as
    auspice always displays the branch from a non-hidden node to a (parent)
    hidden node.
    
    (2) modify the num_date & div attributes for hidden nodes such that
    they do not take up screen space in auspice. I.e. they are modified so
    that they are equal to the minimum values of non-hidden nodes in the tree.

--- scripts/annotate_hidden_nodes.py
+++ scripts/annotate_hidden_nodes.py
@@ -42,6 +42,25 @@ def get_cluster(node):
     except KeyError:
         return False
 
+def make_stub(node):
+    
+    if "cluster" not in node["attr"] or node["attr"]["cluster"] == False:
+        return node
+
+    stub = {
+        "strain": "stub_{}".format(get_cluster(node)),
+        "attr": {
+            "num_date": node["attr"]["num_date"],
+            "div": node["attr"]["div"]
+        },
+        "hidden": "always",
+        "children": [node],
+        "clade": int("100000{}".format(get_cluster(node))) # required in auspice v1
+    }
+
+    return stub
+
+
 def mark_clusters_as_hidden(node):
     if not "children" in node:
         return # ignore terminal nodes
@@ -50,6 +69,46 @@ def mark_clusters_as_hidden(node):
         node["attr"]["cluster"] = child_clusters.pop()
     else:
         node["hidden"] = "always"
+        # the children here will each lead to a different cluster
+        # and we want to make them "stubs", i.e. not show the long
+        # branch length leading to the start of the cluster
+        node["children"] = [make_stub(child) for child in node["children"]]
+
+
+def flatten_tree(tree):
+    flat = []
+    stack = [tree]
+    while(len(stack) > 0):  
+        node = stack.pop()
+        flat.append(node)
+        if "children" in node:
+            for child in node["children"]:
+                stack.append(child)
+    return flat
+
+
+def shift_hidden_div_and_time(flat_tree):
+    """
+        Shift the num_date and div attributes of the hidden nodes
+        so that the non-hidden nodes take up the entire screen in
+        auspice. (Auspice calculates the view based on all the nodes,
+        whether they are hidden or not, so a deep MRCA -- even if
+        hidden -- doesn't look great.)
+
+        This uses the trick that both flat_tree & tree contain references
+        to the same object, so modifications to one can affect the other
+    """
+    min_date = min([n["attr"]["num_date"] for n in flat_tree if get_cluster(n)])
+    min_div = min([n["attr"]["div"] for n in flat_tree if get_cluster(n)]) # cumulative!
+    for n in flat_tree:
+        if n["attr"]["num_date"] < min_date:
+            n["attr"]["num_date"] = min_date
+            if "num_date_confidence" in n["attr"]:
+                del n["attr"]["num_date_confidence"]
+        if n["attr"]["div"] < min_div:
+            n["attr"]["div"] = 0
+        else:
+            n["attr"]["div"] -= min_div
 
 
 if __name__ == '__main__':
@@ -59,6 +118,8 @@ if __name__ == '__main__':
         tree = json.load(fh)
 
     post_order_traversal_iterative(tree, mark_clusters_as_hidden)
+    flat_tree = flatten_tree(tree)
+    shift_hidden_div_and_time(flat_tree)
 
     with open(args.output, "w") as fh:
         json.dump(tree, fh, indent=2, sort_keys=True)
