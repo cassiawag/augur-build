ebc6eb71a7305b7f3f1b8f719d3a3ecd2f520c8a
Cassia Wagner <cassia.wagner9@gmail.com>
2019-07-15 17:08:30 -0700

    Removes other source features from gb reference

--- scripts/create_ref_genome.py
+++ scripts/create_ref_genome.py
@@ -26,14 +26,23 @@ def concat(files, reference):
     flu_ref.id = reference
 
     #Adds source feature to Genbank file for the full-length of genome.
-    source_feature = flu_ref.features[0]
-    full_genome_feature = SeqFeature(FeatureLocation(0, len(flu_ref), strand = 1), type = "source", qualifiers = source_feature.qualifiers)
+    model_source_feature = flu_ref.features[0]
+    full_genome_feature = SeqFeature(FeatureLocation(0, len(flu_ref), strand = 1), type = "source", qualifiers = model_source_feature.qualifiers)
     #Removes qualifiers that don't apply to the entire sequence.
     if "segment" in full_genome_feature.qualifiers.keys():
         del full_genome_feature.qualifiers["segment"]
     if "lab_host" in full_genome_feature.qualifiers.keys():
         del full_genome_feature.qualifiers["lab_host"]
     flu_ref.features.insert(0, full_genome_feature)
+
+    #Removes features of type source that don't encompass whole genome.
+    source_features = []
+    for feature in flu_ref.features:
+        if feature.type == "source" and flu_ref.features.index(feature) != 0:
+            source_features.append(flu_ref.features.index(feature))
+    source_features.reverse()
+    for index in source_features:
+        flu_ref.features.pop(index)
 
     return flu_ref
 
